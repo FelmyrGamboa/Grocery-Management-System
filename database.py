14b68d5a0fb16d1380688c0eabb29388296982bc
FelmyrGamboa <felmyrgamboa23@gmail.com>
2025-01-16 00:25:02 +0800

    Modified database to a better version and organization

--- database.py
+++ database.py
@@ -1,14 +1,51 @@
 import mysql.connector
 
 class Customer:
-    db = mysql.connector.connect(
-        host = "localhost",
-        user = "root",
-        passwd = "mysql1234",
-        database = "customers_information_database"
-    )
+    def __init__(self):
+        self.db = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            passwd = "mysql1234",
+            database = "customers_information_database"
+        )
+        self.customer_cursor = self.db.cursor()
 
-    customer_cursor = db.cursor()
+    def create_database(self):
+        self.customer_cursor("CREATE DATABASE customers_information_database")
+
+    def create_table_customer_information(self):
+        self.customer_cursor("""CREATE TABLE customer_information (
+                            customerID INT PRIMARY KEY AUTO_INCREMENT,
+                            fullname VARCHAR(200) NOT NULL, 
+                            username VARCHAR(200) NOT NULL UNIQUE,
+                            password VARCHAR(200) NOT NULL
+                             """)
+        
+    def create_table_shopping_history(self):
+        self.customer_cursor("""CREATE TABLE customer_shopping_history (
+                            product_name VARCHAR(200) NOT NULL,
+                            price DECIMAL(10,2) NOT NULL, 
+                            quantity INT UNSIGNED NOT NULL, 
+                            total_price DECIMAL (10,2) NOT NULL,
+                            date_time DATETIME
+                             """)
+        
+db_table = Customer()
+# db_table.create_database()
+# db_table.create_table_customer_information()
+# db_table.create_table_shopping_history()
+
+
+
+
+    # db = mysql.connector.connect(
+    #     host = "localhost",
+    #     user = "root",
+    #     passwd = "mysql1234",
+    #     database = "customers_information_database"
+    # )
+
+    # customer_cursor = db.cursor()
     # customer_cursor.execute("CREATE DATABASE customers_information_database")
 
     # customer_cursor.execute("""CREATE TABLE customer_information(
@@ -26,4 +63,4 @@ class Customer:
     #                         date_time DATETIME
     #                         )""")
 
-    customer_cursor.execute("ALTER TABLE")
+    # customer_cursor.execute("ALTER TABLE")
