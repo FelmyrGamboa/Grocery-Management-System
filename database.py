e8275046d4b8e406abb46dc4a1ff4bebec571cdb
FelmyrGamboa <felmyrgamboa23@gmail.com>
2025-01-18 22:55:52 +0800

    Fixed errors in running database

--- database.py
+++ database.py
@@ -6,64 +6,37 @@ class Customer:
             host = "localhost",
             user = "root",
             passwd = "mysql1234",
-<<<<<<< HEAD
             database = "customers_information_database"
-=======
-            database = "customers_information_database",
->>>>>>> a08497f9d78f7b155431074073bfd4ad805686eb
         )
         self.customer_cursor = self.db.cursor()
 
     def create_database(self):
-<<<<<<< HEAD
-        self.customer_cursor("CREATE DATABASE customers_information_database")
-
-    def create_table_customer_information(self):
-        self.customer_cursor("""CREATE TABLE customer_information (
-=======
         self.customer_cursor.execute("CREATE DATABASE customers_information_database")
 
     def create_table_customer_information(self):
         self.customer_cursor.execute("""CREATE TABLE customer_information (
->>>>>>> a08497f9d78f7b155431074073bfd4ad805686eb
                             customerID INT PRIMARY KEY AUTO_INCREMENT,
                             fullname VARCHAR(200) NOT NULL, 
                             username VARCHAR(200) NOT NULL UNIQUE,
                             password VARCHAR(200) NOT NULL
-<<<<<<< HEAD
                              """)
         
     def create_table_shopping_history(self):
-        self.customer_cursor("""CREATE TABLE customer_shopping_history (
-=======
-<<<<<<< Updated upstream
-                             )""")
-=======
-                            )""")
->>>>>>> Stashed changes
-        
-    def create_table_shopping_history(self):
         self.customer_cursor.execute("""CREATE TABLE customer_shopping_history (
->>>>>>> a08497f9d78f7b155431074073bfd4ad805686eb
                             product_name VARCHAR(200) NOT NULL,
                             price DECIMAL(10,2) NOT NULL, 
                             quantity INT UNSIGNED NOT NULL, 
                             total_price DECIMAL (10,2) NOT NULL,
                             date_time DATETIME
-<<<<<<< HEAD
                              """)
-=======
-<<<<<<< Updated upstream
-                             )""")
-=======
-                            )""")
->>>>>>> Stashed changes
->>>>>>> a08497f9d78f7b155431074073bfd4ad805686eb
         
 db_table = Customer()
 # db_table.create_database()
 # db_table.create_table_customer_information()
 # db_table.create_table_shopping_history()
+
+
+
 
 
 
@@ -76,14 +49,14 @@ db_table = Customer()
     # )
 
     # customer_cursor = db.cursor()
-    # customer_cursor.execute("CREATE DATABASE customers_information_database")
+    # # customer_cursor.execute("CREATE DATABASE customers_information_database")
 
-    # customer_cursor.execute("""CREATE TABLE customer_information(
-    #                         customerID INT PRIMARY KEY AUTO_INCREMENT,
-    #                         fullname VARCHAR(200) NOT NULL, 
-    #                         username VARCHAR(200) NOT NULL UNIQUE,
-    #                         password VARCHAR(200) NOT NULL
-    #                         )""")
+    # # customer_cursor.execute("""CREATE TABLE customer_information(
+    # #                         customerID INT PRIMARY KEY AUTO_INCREMENT,
+    # #                         fullname VARCHAR(200) NOT NULL, 
+    # #                         username VARCHAR(200) NOT NULL UNIQUE,
+    # #                         password VARCHAR(200) NOT NULL
+    # #                         )""")
 
     # customer_cursor.execute("""CREATE TABLE customer_shopping_history(
     #                         product_name VARCHAR(200) NOT NULL,
@@ -93,4 +66,9 @@ db_table = Customer()
     #                         date_time DATETIME
     #                         )""")
 
-    # customer_cursor.execute("ALTER TABLE")
+    # def signup(fullname, username, password):
+    #     Customer.customer_cursor.execute("INSERT INTO customer_information (fullname, username, password) VALUES (%s, %s, %s)", (fullname, username, password))
+    #     Customer.db.commit()
+
+    # def login(username, password):
+    #     Customer.customer_cursor.execute("")
