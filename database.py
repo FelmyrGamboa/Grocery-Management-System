32b744b4fb3c9d285c125688cc6df24f940df59e
FelmyrGamboa <felmyrgamboa23@gmail.com>
2025-01-25 00:45:47 +0800

    Fixed errors in running database

--- database.py
+++ database.py
@@ -19,21 +19,63 @@ class Customer:
                             fullname VARCHAR(200) NOT NULL, 
                             username VARCHAR(200) NOT NULL UNIQUE,
                             password VARCHAR(200) NOT NULL
-                             """)
+                             )""")
         
     def create_table_shopping_history(self):
         self.customer_cursor.execute("""CREATE TABLE customer_shopping_history (
+                            username VARCHAR(200) NOT NULL,
                             product_name VARCHAR(200) NOT NULL,
                             price DECIMAL(10,2) NOT NULL, 
                             quantity INT UNSIGNED NOT NULL, 
                             total_price DECIMAL (10,2) NOT NULL,
                             date_time DATETIME
-                             """)
+                             )""")
+        
+    def drop_table(self):
+        self.customer_cursor.execute("DROP TABLE customer_shopping_history")
         
 db_table = Customer()
 # db_table.create_database()
 # db_table.create_table_customer_information()
 # db_table.create_table_shopping_history()
+
+
+
+
+
+
+
+    # db = mysql.connector.connect(
+    #     host = "localhost",
+    #     user = "root",
+    #     passwd = "mysql1234",
+    #     database = "customers_information_database"
+    # )
+
+    # customer_cursor = db.cursor()
+    # # customer_cursor.execute("CREATE DATABASE customers_information_database")
+
+    # # customer_cursor.execute("""CREATE TABLE customer_information(
+    # #                         customerID INT PRIMARY KEY AUTO_INCREMENT,
+    # #                         fullname VARCHAR(200) NOT NULL, 
+    # #                         username VARCHAR(200) NOT NULL UNIQUE,
+    # #                         password VARCHAR(200) NOT NULL
+    # #                         )""")
+
+    # customer_cursor.execute("""CREATE TABLE customer_shopping_history(
+    #                         product_name VARCHAR(200) NOT NULL,
+    #                         price DECIMAL(10,2) NOT NULL, 
+    #                         quantity INT UNSIGNED NOT NULL, 
+    #                         total_price DECIMAL (10,2) NOT NULL,
+    #                         date_time DATETIME
+    #                         )""")
+
+    # def signup(fullname, username, password):
+    #     Customer.customer_cursor.execute("INSERT INTO customer_information (fullname, username, password) VALUES (%s, %s, %s)", (fullname, username, password))
+    #     Customer.db.commit()
+
+    # def login(username, password):
+    #     Customer.customer_cursor.execute("")
 
 
 
