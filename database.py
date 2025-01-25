fbd7c6beacd1dd3704904438630b2aeaa441582c
erichestella <erichestella05@gmail.com>
2025-01-25 10:05:35 +0800

    ewan basta upload lang

--- database.py
+++ database.py
@@ -11,29 +11,33 @@ class Customer:
         self.customer_cursor = self.db.cursor()
 
     def create_database(self):
-        self.customer_cursor("CREATE DATABASE customers_information_database")
+        self.customer_cursor.execute("CREATE DATABASE customers_information_database")
 
     def create_table_customer_information(self):
-        self.customer_cursor("""CREATE TABLE customer_information (
+        self.customer_cursor.execute("""CREATE TABLE customer_information (
                             customerID INT PRIMARY KEY AUTO_INCREMENT,
                             fullname VARCHAR(200) NOT NULL, 
                             username VARCHAR(200) NOT NULL UNIQUE,
                             password VARCHAR(200) NOT NULL
-                             """)
+                             )""")
         
     def create_table_shopping_history(self):
-        self.customer_cursor("""CREATE TABLE customer_shopping_history (
+        self.customer_cursor.execute("""CREATE TABLE customer_shopping_history (
                             product_name VARCHAR(200) NOT NULL,
                             price DECIMAL(10,2) NOT NULL, 
                             quantity INT UNSIGNED NOT NULL, 
                             total_price DECIMAL (10,2) NOT NULL,
                             date_time DATETIME
-                             """)
+                             )""")
+#ADDED SOME KILO      
+    def add_column_shopping_history(self):
+        self.customer_cursor.execute("ALTER TABLE customer_shopping_history DROP kilo")
         
 db_table = Customer()
 # db_table.create_database()
 # db_table.create_table_customer_information()
 # db_table.create_table_shopping_history()
+# db_table.add_column_shopping_history()
 
 
 
